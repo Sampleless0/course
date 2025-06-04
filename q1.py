16ef6690fb6963fd49882a26eda6931812d21ab7
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-04 18:36:50 +0000

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,14 @@
+z = 1.25
+y = "a"
+x = 4
+w = 2
+
+print(5 / 4 * z)
+print(y * x)
+print(w + x + z)
+print(z > 1)
+
+print(type(5 / 4 * z))
+print(type(y * x))
+print(type(w + x + z))
+print(type(z > 1))
