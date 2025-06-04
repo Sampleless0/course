1072872dc144523ec6d094cc8c2bfb6ca07faa53
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-04 18:40:20 +0000

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,6 @@
+name = input("enter your name: ")
+age = input("enter your age: ")
+weight = input("enter your weight: ")
+
+print(f"your name is {name}, your age is {age} years old and your weight is {weight} kg.")
+
