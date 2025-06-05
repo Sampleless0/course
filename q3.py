e32adcc5ccab8c1bc0d6998181cd257ed445bf66
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-05 02:56:58 +0000

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,6 @@
+score = int(input("enter your score -> "))
+pass_score = 40
+if score >= pass_score:
+    print("congratulations, you passed")
+else:
+    print("try again, u will do better next time")
