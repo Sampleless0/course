71c941323c25072774a9d0c8d8aaae58b8f7fd87
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 03:15:04 +0000

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,10 +1,13 @@
-def calculateAvg(studentScore):
-    studentScore = sum(studentScore) / len(studentScore)
-    return studentScore
+# receive inputs as float
+first = float(input("first student score -> ")
+second = float(input("second student score -> ")
+third = float(input("third student score -> ")
 
-avg = calculateAvg([float(input("first student score -> ")),float(input("second student score -> ")),float(input("third student score -> "))])
-
+# calculate average and print as 1dp
+avg = (first + second + third) / 3
 print(f'average score: {avg:.1f}')
+
+# if else to check if average >95
 if avg > 95:
     print("congratulations. excellent performance")
 else:
