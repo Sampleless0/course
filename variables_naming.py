3ea6719b10477968f20d39e5326d18f6e2ed9e74
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 07:28:09 +0000

    Update variables_naming.py

--- variables_naming.py
+++ variables_naming.py
@@ -12,7 +12,7 @@ name_ = True # allowed
 name% = False # NOT allowed
 
 def = True # NOT allowed as def is a reserved keyword
-# some reserved keywords that u should know about
+# some reserved keywords that u should know about (u cannot name ur variables as these)
 False      await      else       import     pass
 None       break      except     in         raise
 True       class      finally    is         return
