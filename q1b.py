32ccce5f5c3ecbb4b41dbceea45da02eef69dc62
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 03:09:27 +0000

    Update q1b.py

--- q1b.py
+++ q1b.py
@@ -1,19 +1,24 @@
-def day(o):
-    if o == "monday":
-        return "lunes"
-    elif o == "tuesday":
-        return "martes"
-    elif o == "wednesday":
-        return "miercoles"
-    elif o == "thursday":
-        return "jueves"
-    elif o == "friday":
-        return "viernes"
-    elif o == "saturday":
-        return "sabado"
-    elif o == "sunday":
-        return "domingo"
-    else:
-        return "that is not a valid day of the week"
+# get input
+day = input("gib me day -> ").lower()
+result = ""
 
-print(day(input("gib me day -> ").lower()))
+# if else
+if day == "monday":
+    result = "lunes"
+elif day == "tuesday":
+    result = "martes"
+elif day == "wednesday":
+    result = "miercoles"
+elif day == "thursday":
+    result = "jueves"
+elif day == "friday":
+    result = "viernes"
+elif day == "saturday":
+    result = "sabado"
+elif day == "sunday":
+    result = "domingo"
+else:
+    result = "that is not a valid day of the week"
+
+# print result
+print(result)
