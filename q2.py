c3ee436cdea84327694c74114f4d5cdd1b98694a
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 01:56:25 +0000

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,18 +1,13 @@
-def isLeftClear():
-    #code to check if left side is clear
-    return True
-
+# receive input
 robotPathStatus = input("what is the robot's path status (clear, obstacle, destination) -> ").lower()
 battery = int(input("input battery percentage -> "))
 
+# check for battery and input while printing result
 if robotPathStatus == "destination":
     print("stopped at destination")
 elif robotPathStatus == "clear" and battery > 20:
     print("moving forward")
 elif robotPathStatus == "obstacle" and battery > 20:
-    if isLeftClear():
-        print("turning left")
-    else:
-        print("turning right")
+    print("turn left or right")
 else:
     print("invalid input or battery insufficient")
