7fd30e7d42891f84d474353c69f99c2f3321348d
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 01:54:12 +0000

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,15 +1,17 @@
-student = [float(input("first test score -> ")), \
-           float(input("second test score -> ")), \
-           float(input("third test score -> "))]
+# input variables as float
+first = float(input("first test score -> "))
+second = float(input("second test score -> "))
+third = float(input("third test score -> "))
 
-def calculateSuccess(score):
-    avg = sum(score) / len(score)
-    if avg < 85:
-        return "not eligible"
-    else:
-        for i in score:
-            if i < 50:
-                return "not eligible"
-    return "eligible"
+# calculate average
+avg = (first + second + third) / 3
 
-print(calculateSuccess(student))
+# check for eligiblility
+eligible = ""
+if avg < 85 or first < 50 or second < 50 or third < 50:
+           eligible = "not eligible"
+else:
+           eligible = "eligible"
+
+# print result
+print(eligible)
