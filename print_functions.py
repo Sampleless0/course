b8857176b45ff98861fa1a9af8e76808289686d3
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 05:37:39 +0000

    Create print_functions.py

---
+++ print_functions.py
@@ -0,0 +1,12 @@
+# print functions lol
+
+print('hello world') # ' ' single quote (hello world)
+print("hello world") # " " double quote (hello world)
+
+# printing ' ' or " " inside a print function
+print("hello 'world'") # hello 'world'
+print('hello "world"') # hello "world"
+
+# another way to do that is with \' or \"
+print('hello \'world\'') # hello 'world'
+print("hello \"world\"") # hello "world"
