7b077d7321db6dbabcec890fa37c10473f7e59b5
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 02:10:51 +0000

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,4 +1,7 @@
+# uses math function
 import math
+
+# function to print menu
 def display_menu():
     print("")
     print('Geometry calculator menu')
@@ -9,28 +12,38 @@ def display_menu():
     print('5) Quit')
     print("")
 
+# function to calculate area of circle (takes in radius)
 def area_circle(radius):
     return math.pi * radius ** 2
 
+# function to calculate circumference of circle (takes in radius)
 def circle_circumference(radius):
     return 2 * math.pi * radius
 
+# function to calculate area of rectangle (takes in length and width)
 def area_rectangle(length, width):
     return length * width
 
+# function to calculate perimeter of rectangle (takes in length and width)
 def perimeter_rectangle(length, width):
     return 2 * (length + width)
 
+# function for main code
 def main():
     choice = 0
+
+    # while loop that lets users repeat the calculations until they quit
     while choice != 5:
         display_menu()
+
+        # tries to receive choice as int
         try:
             choice = int(input('Enter your choice: '))
         except Exception:
             print("invalid input")
             break
 
+        # ifelse statements to calculate results
         if choice == 1:
             radius = float(input("Enter the circle's radius: "))
             print(f'\nThe area of the circle = {area_circle(radius)}')
@@ -51,4 +64,6 @@ def main():
         else:
             print('Error: invalid selection.')
             break
+
+# run main function
 main()
