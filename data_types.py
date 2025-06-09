27c91c3dd3c77028b3bd747f3dfa93498dbd0e87
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 05:21:20 +0000

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,27 @@
+# data types lol
+
+# boolean (true/false)
+bool1 = True
+bool2 = False
+
+# string (words letters etc)
+string1 = "hello world 101"
+
+# integer (whole numbers | negative numbers)
+int1 = 0
+int2 = 100
+int3 = -100
+
+# float (numbers with decimal points)
+float1 = 10.01
+float2 = -10.01
+
+
+print(f"bool1 (true): {bool1}")
+print(f"bool2 (false): {bool2}")
+print(f"string: {string1}")
+print(f"int1 (zero): {int1}")
+print(f"int2 (positive): {int2}")
+print(f"int3 (negative): {int3}")
+print(f"float1 (positive): {float1}")
+print(f"float2 (negative): {float2}")
