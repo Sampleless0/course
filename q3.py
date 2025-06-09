74cfceb8a21b226bfee8e8808a65076b08a1727a
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 01:20:54 +0000

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,9 +1,8 @@
+# receives input as float
 item1 = float(input("cost of item 1 = "))
 item2 = float(input("cost of item 2 = "))
 
-def calculate(firstItem, secondItem):
-    print(f"cost before gst = ${firstItem + secondItem:.2f}")
-    print(f"cost of gst = ${(firstItem + secondItem)*0.08:.2f}")
-    print(f"cost after gst = ${(firstItem + secondItem)*1.08:.2f}")
-
-calculate(item1, item2)
+# print results to 2dp
+print(f"cost before gst = ${item1 + item2:.2f}")
+print(f"cost of gst = ${(item1 + item2)*0.08:.2f}")
+print(f"cost after gst = ${(item1 + item2)*1.08:.2f}")
