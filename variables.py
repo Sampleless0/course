4ad8d5cfca4180993e4a2c0011320ac184f99180
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 05:34:41 +0000

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,6 +2,24 @@
 
 # naming variables
 
-hello = True # allowed
-5World = False # NOT allowed as numbers cannot be at the front
-World5 = True # allowed
+name = True # allowed
+
+1name = False # NOT allowed as numbers cannot be at the front
+name1 = True # allowed
+
+_name = True # allowed
+name_ = True # allowed
+
+%name = False # NOT allowed as only _ can be used
+name% = False # NOT allowed
+
+def = True # NOT allowed as def is a reserved keyword
+
+# some reserved keywords that u should know about
+False      await      else       import     pass
+None       break      except     in         raise
+True       class      finally    is         return
+and        continue   for        lambda     try
+as         def        from       nonlocal   while
+assert     del        global     not        with
+async      elif       if         or         yield
