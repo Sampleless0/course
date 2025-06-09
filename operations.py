ed739a8a3fc8fac1dacf98fd732bc95e7990b00c
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 05:50:23 +0000

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,29 @@
+# operations
+
+# setting variable
+x = 1
+a = b = c = 1 # (can be like this too)
+
+# addition (both means the same)
+x = x + 1
+x += 1
+
+# subtraction (both means the same)
+x = x - 1
+x -= 1
+
+# multiplication
+x = x * 2
+x *= 2
+
+# division (converts int to float)
+x = x / 2
+x /= 2
+
+# floor division (round down to nearest integer)
+x = x // 2
+x //= 2
+
+# modulus (finds the remainder)
+x = x % 2
+x %= 2
