8d6a9a747a9ca884f572f4e7505a221c36faf8ce
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 01:16:30 +0000

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,11 +1,11 @@
+# list variables
 current = 0
 future = 10000
 interest = 0.05
 years = 10
 
-def amountRequired():
-    current = future/((1+interest)**years)
-    print(f"You need to deposit today: ${current:.2f}")
+# calculate for compound interest
+current = future/((1+interest)**years)
 
-
-amountRequired()
+# print result
+print(f"You need to deposit today: ${current:.2f}")
