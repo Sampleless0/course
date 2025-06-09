6713359c1b27e16f076020c05f88629b3401273a
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 01:58:32 +0000

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,4 +1,7 @@
+# receive input as string
 inputPin = input("Enter 4 digit PIN: ")
+
+# print result with if else
 if inputPin == "1234":
     amount = int(input("withdrawal amount: "))
     if amount <= 500:
