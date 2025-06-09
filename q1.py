58f89d1ac22270115899d8a94d7bccfbfdb54930
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 02:15:22 +0000

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,15 +1,21 @@
+# import random module
 import random
 
+# gets a random integer between 1 to 10000
 num1 = random.randint(1, 10000)
 num2 = random.randint(1, 10000)
 
+# forever loop
 while True:
+    # tries to get an input as int
     try:
         guess = int(input(f"whats {num1} + {num2}: "))
     except Exception:
         print("sorry, try again")
         continue
     else:
+        
+        # check if ans is correct
         if guess == (num1 + num2):
             print("you guessed right")
             break
