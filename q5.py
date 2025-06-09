afd3ab6ecdecc3ef2e62dd803cd13bcf40644228
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 01:35:17 +0000

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,14 +1,15 @@
-cookie = {
-    "suger": 1.5/48,
-    "butter": 1/48,
-    "flour": 1/24
-}
+# list variables
+suger = 1.5/48
+butter = 1/48
+flour = 1/24
+
+# ask for number of cookies
 wantCookie = int(input("how many cookies u want = "))
 
-def calculate(wantCookie, cookie):
-    suger = wantCookie * cookie["suger"]
-    butter = wantCookie * cookie["butter"]
-    flour = wantCookie * cookie["flour"]
-    return str(f"u need {suger} cups of suger, {butter} cups of butter and {flour} cups of flour")
+# calculate ingredients
+suger *= wantCookie
+butter *= wantCookie
+flour *= wantCookie
 
-print(calculate(wantCookie, cookie))
+# print result
+print(f"u need {suger} cups of suger, {butter} cups of butter and {flour} cups of flour")
