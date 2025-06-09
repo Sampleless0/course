cb475f20934c58fc9ab63395e89b39a0f1796786
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-06-09 03:12:27 +0000

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,9 +1,7 @@
-def weightOK(weight):
-    if weight >= 25:
-        return True
-    return False
+# get input
+weight = int(input("how many kilograms does your luggage weight -> "))
 
-if weightOK(int(input("how many kilograms does your luggage weight -> "))):
-    print("there is an extra charge of $20")
-
+# check for weight and print result
+if weight >= 25:
+     print("there is an extra charge of $20")
 print("thank you")
