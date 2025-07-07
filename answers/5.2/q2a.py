652bd9376b53c2a5f3c3d3aa86e8ccd6ea759549
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-07-07 06:29:51 +0000

    Create q2a.py

---
+++ answers/5.2/q2a.py
@@ -0,0 +1,7 @@
+# list people
+people = [
+    {"name": "Mary Tan", "nric": "S1234567A"},
+    {"name": "Steve Lim", "nric": "S2340986T"},
+    {"name": "Yeo Ming Ming", "nric": "S6734278E"},
+    {"name": "Aaron Smith", "nric": "G7878439K"}
+]
