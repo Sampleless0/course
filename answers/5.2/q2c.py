08c1c9784052b48e30ad2576e39d6f7c4cce44ad
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-07-07 06:36:40 +0000

    Create q2c.py

---
+++ answers/5.2/q2c.py
@@ -0,0 +1,11 @@
+# list people
+people = [
+    {"name": "Mary Tan", "nric": "S1234567A"},
+    {"name": "Steve Lim", "nric": "S2340986T"},
+    {"name": "Yeo Ming Ming", "nric": "S6734278E"},
+    {"name": "Aaron Smith", "nric": "G7878439K"},
+    {"name": "Lam Lin Shan", "nric": "S4456223G"}
+]
+
+for person in people:
+  print(f'Name: {person["name"]}, NRIC: {person["nric"]}')
