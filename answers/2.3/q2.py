9996b907e9a90c764b09aac8b8e82709d38ea7c0
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-07-07 06:07:40 +0000

    Create q2.py

---
+++ answers/2.3/q2.py
@@ -0,0 +1,7 @@
+# get input
+weight = int(input("how many kilograms does your luggage weight -> "))
+
+# check for weight and print result
+if weight >= 25:
+     print("there is an extra charge of $20")
+print("thank you")
