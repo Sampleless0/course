ce4e9f37c2f2caf165fb746c4f981e33941c8d88
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-07-07 06:03:36 +0000

    Update and rename q1 to q1.py

---
+++ answers/2.1/q1.py
@@ -0,0 +1,17 @@
+# set variables
+z = 1.25
+y = "a"
+x = 4
+w = 2
+
+# print variables
+print(5 / 4 * z)
+print(y * x)
+print(w + x + z)
+print(z > 1)
+
+# print type of variable (eg. string, int, float, bool)
+print(type(5 / 4 * z))
+print(type(y * x))
+print(type(w + x + z))
+print(type(z > 1))
