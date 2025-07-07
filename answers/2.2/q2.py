38e8401b1848850767be4329d6b29695a48941fc
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-07-07 06:05:16 +0000

    Create q2.py

---
+++ answers/2.2/q2.py
@@ -0,0 +1,6 @@
+# save inputs as integer
+numStudents = int(input("number of students = "))
+numPass = int(input("number of pass = "))
+
+# print results to %
+print(f"percentage of passed = {(numPass/numStudents):.2%}")
