daf29e26454f9c08a325fea2abee739f5441cb33
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-07-07 06:05:00 +0000

    Create q1.py

---
+++ answers/2.2/q1.py
@@ -0,0 +1,7 @@
+# asking for string inputs
+name = input("enter your name: ")
+age = input("enter your age: ")
+weight = input("enter your weight: ")
+
+# print everything use f-strings
+print(f"your name is {name}, your age is {age} years old and your weight is {weight} kg.")
