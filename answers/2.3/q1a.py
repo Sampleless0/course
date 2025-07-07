9fc6e2180a07ebb99c5f6c4b12d5a139b5b8b4a2
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-07-07 06:07:06 +0000

    Create q1a.py

---
+++ answers/2.3/q1a.py
@@ -0,0 +1,12 @@
+# get input
+value = int(input("gib me number -> "))
+result = ""
+
+# check for value
+if value <= 0 // value > 100:
+    result = "out of range"
+else: 
+    result = "ok"
+
+# print result
+print(result)
