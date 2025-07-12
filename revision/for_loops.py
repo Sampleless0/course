1cbc31c1fa2384bd9e70fd644cef841c721b44f1
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-07-12 13:52:55 +0000

    Create for_loops.py

---
+++ revision/for_loops.py
@@ -0,0 +1,29 @@
+# for loops
+
+
+# basic for loop
+for i in range(1,6)
+  print(i)
+# expected:
+# 1
+# 2
+# 3
+# 4
+# 5
+# program ended.
+
+# the "i" could be any variable
+for number in range(1,6)
+  print(number)
+# expected:
+# 1
+# 2
+# 3
+# 4
+# 5
+# program ended.
+
+
+
+# for loops using arrays
+numbers = [1,10,100,1000]
