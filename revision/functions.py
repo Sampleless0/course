d19f32e2d724014929190c06d446b9887d8b6df0
da duck <100190797+Sampleless0@users.noreply.github.com>
2025-07-13 16:01:42 +0000

    Create functions.py

---
+++ revision/functions.py
@@ -0,0 +1,21 @@
+# functions
+
+
+# built in functions
+print() # print a result
+len() # returns length of string, list or tuple
+type() # finds the type of variable (bool, int, string etc)
+range() # from a number to another number
+input() # user type in smt
+
+
+
+# write new function
+def function_name():
+  print("it ran")
+
+# run the function
+function_name()
+# expected:
+# it ran
+# program ended.
